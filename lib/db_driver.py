aee66f8b2d3738585ca0f28369207356a136f331
Sundar Shrestha <plsth@icloud.com>
2024-11-24 21:14:51 -0600

    updated db_driver.py

--- lib/db_driver.py
+++ lib/db_driver.py
@@ -4,7 +4,7 @@ import sqlite3
 class Connection:
     
     def __init__(self):
-        self.conn = self.db_connection()
+        self.cursor = self.db_connection().cursor()
 
     def db_connection(self):
         try:
@@ -14,13 +14,9 @@ class Connection:
             print(" Failed to open database: ", e)
     
     def login(self, email, password):
-        conn = self.conn
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password)).close()
-        result = cursor.fetchone()
+        self.cursor.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+        result = self.cursor.fetchone()
 
     def create_database(self):
-        conn = self.conn
-        cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, address TEXT NOT NULL, phone TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, address TEXT NOT NULL, phone TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL)''')
         print('Successfully created new table')
